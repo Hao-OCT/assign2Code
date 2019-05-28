674a4b2c0a654c214ffda4071097e6c40b58fa34
Hao-OCT <patrick.wang1025@gmail.com>
2019-05-28 11:25:30 +1000

    Task C

--- pathFinder/DijkstraPathFinder.java
+++ pathFinder/DijkstraPathFinder.java
@@ -26,10 +26,54 @@ public class DijkstraPathFinder implements PathFinder {
         //Create a priority queue to store the node in the order of cost.
         PriorityQueue<Node> pq = new PriorityQueue<>(nodeComparator);
         List<Node> nodeList = new ArrayList<>();
+        int min = map.sizeC * map.sizeR - map.numberOfImpassable();
+        int indexOfOrigin = -1;
+        int indexOfDest = -1;
+        for (int i = 0; i < map.originCells.size(); i++) {
+            Node current = new Node(null, map.originCells.get(i), 0);
+            for (int j = 0; j < map.destCells.size(); j++) {
+                Node dest = new Node(null, map.destCells.get(j), 0);
 
-        Node current = new Node(null, map.originCells.get(0), 0);
-        Node dest = new Node(null, map.destCells.get(0), 0);
+                while (!current.equals(dest)) {
+                    int r = current.getCoordinate().getRow();
+                    int c = current.getCoordinate().getColumn();
 
+                    Coordinate up = new Coordinate(r + 1, c);
+                    Coordinate down = new Coordinate(r - 1, c);
+                    Coordinate left = new Coordinate(r, c - 1);
+                    Coordinate right = new Coordinate(r, c + 1);
+
+                    checkNode(pq, nodeList, up, current);
+                    checkNode(pq, nodeList, down, current);
+                    checkNode(pq, nodeList, left, current);
+                    checkNode(pq, nodeList, right, current);
+
+                    if (!pq.isEmpty()) {
+                        nodeList.add(current);
+                        current = pq.remove();
+                    } else break;
+                }
+                if (current.equals(dest)) {
+                    do {
+                        path.add((current.getCoordinate()));
+                        current = current.getOrigin();
+                    } while (current.getOrigin() != null);
+
+                    //put the origin into the path
+                    path.add(map.originCells.get(i));
+                    if (path.size() < min) {
+                        min = path.size();
+                        indexOfOrigin = i;
+                        indexOfDest = j;
+                    }
+                    path.clear();
+                    nodeList.clear();
+                    pq.clear();
+                }
+            }
+        }
+        Node current = new Node(null, map.originCells.get(indexOfOrigin), 0);
+        Node dest = new Node(null, map.destCells.get(indexOfDest), 0);
         while (!current.equals(dest)) {
             int r = current.getCoordinate().getRow();
             int c = current.getCoordinate().getColumn();
@@ -56,8 +100,7 @@ public class DijkstraPathFinder implements PathFinder {
             } while (current.getOrigin() != null);
 
             //put the origin into the path
-            path.add(map.originCells.get(0));
-
+            path.add(map.originCells.get(indexOfOrigin));
             Collections.reverse(path);
         }
         // TODO: Implement
