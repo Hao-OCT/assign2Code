a0ac2b308c6802632b51677f726552a17e42c6a3
qiuyucc <china.cqy@outlook.com>
2019-05-28 15:10:07 +1000

    updated for task c

--- src/pathFinder/DijkstraPathFinder.java
+++ src/pathFinder/DijkstraPathFinder.java
@@ -5,6 +5,7 @@ import map.PathMap;
 
 import java.util.*;
 
+
 public class DijkstraPathFinder implements PathFinder {
     // TODO: You might need to implement some attributes
     private PathMap map;
@@ -21,60 +22,38 @@ public class DijkstraPathFinder implements PathFinder {
     @Override
     public List<Coordinate> findPath() {
         List<Coordinate> path = new ArrayList<Coordinate>();
-
-        Comparator<Node> nodeComparator = (node1, node2) -> node1.getCost() - node2.getCost();
+        List<List<Coordinate>> potentialPath = new ArrayList<List<Coordinate>>();
+        //Comparator<Node> nodeComparator = (node1, node2) -> node1.getCost() - node2.getCost();
         //Create a priority queue to store the node in the order of cost.
-        PriorityQueue<Node> pq = new PriorityQueue<>(nodeComparator);
-        List<Node> nodeList = new ArrayList<>();
-        int min = map.sizeC * map.sizeR - map.numberOfImpassable();
-        int indexOfOrigin = -1;
-        int indexOfDest = -1;
+        //PriorityQueue<Node> pq = new PriorityQueue<>(nodeComparator);
+        //List<Node> nodeList = new ArrayList<>();
+        int numberOfWayPoint = map.waypointCells.size();
         for (int i = 0; i < map.originCells.size(); i++) {
             Node current = new Node(null, map.originCells.get(i), 1);
             for (int j = 0; j < map.destCells.size(); j++) {
                 Node dest = new Node(null, map.destCells.get(j), 1);
-
-                while (!current.equals(dest)) {
-                    int r = current.getCoordinate().getRow();
-                    int c = current.getCoordinate().getColumn();
-
-                    Coordinate up = new Coordinate(r + 1, c);
-                    Coordinate down = new Coordinate(r - 1, c);
-                    Coordinate left = new Coordinate(r, c - 1);
-                    Coordinate right = new Coordinate(r, c + 1);
-
-                    checkNode(pq, nodeList, up, current);
-                    checkNode(pq, nodeList, down, current);
-                    checkNode(pq, nodeList, left, current);
-                    checkNode(pq, nodeList, right, current);
-
-                    if (!pq.isEmpty()) {
-                        nodeList.add(current);
-                        current = pq.remove();
-                    } else break;
-                }
-                if (current.equals(dest)) {
-                    do {
-                        path.add((current.getCoordinate()));
-                        current = current.getOrigin();
-                    } while (current.getOrigin() != null);
-
-                    //put the origin into the path
-                    path.add(map.originCells.get(i));
-                    if (path.size() < min) {
-                        min = path.size();
-                        indexOfOrigin = i;
-                        indexOfDest = j;
-                    }
-                    path.clear();
-                    nodeList.clear();
-                    pq.clear();
-                }
+                path = findAtoB(current,dest);
+                potentialPath.add(path);
             }
         }
-        Node current = new Node(null, map.originCells.get(indexOfOrigin), 1);
-        Node dest = new Node(null, map.destCells.get(indexOfDest), 1);
-        while (!current.equals(dest)) {
+		Collections.sort(potentialPath, new Comparator<List<Coordinate>>() {
+			@Override
+			public int compare(List<Coordinate> o1, List<Coordinate> o2) {
+				return o1.size() - o2.size();
+			}
+		});
+        return path;
+    } // end of findPath()
+    
+    public List<Coordinate> findAtoB( Node A, Node B) 
+    {
+    	List<Coordinate> path = new ArrayList<Coordinate>();
+    	 Comparator<Node> nodeComparator = (node1, node2) -> node1.getCost() - node2.getCost();
+    	PriorityQueue<Node> pq = new PriorityQueue<>(nodeComparator);
+        List<Node> nodeList = new ArrayList<>();
+    	Node current = A;
+    	Node dest = B;
+    	while (!current.equals(dest)) {
             int r = current.getCoordinate().getRow();
             int c = current.getCoordinate().getColumn();
 
@@ -100,12 +79,12 @@ public class DijkstraPathFinder implements PathFinder {
             } while (current.getOrigin() != null);
 
             //put the origin into the path
-            path.add(map.originCells.get(indexOfOrigin));
+            path.add(A.getCoordinate());
             Collections.reverse(path);
         }
         // TODO: Implement
         return path;
-    } // end of findPath()
+    }
 
     public void checkNode(PriorityQueue<Node> pq, List<Node> nodeList, Coordinate coordinate, Node current) {
         Node temp;
